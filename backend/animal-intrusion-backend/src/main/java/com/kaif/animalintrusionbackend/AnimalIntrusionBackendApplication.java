f5d276d0a49e9661106d52101bf83fa23a1f1b49
kaifkazmi <sayedkaif784@gmail.com>
2026-02-28 17:11:08 +0530

    Initial Spring Boot backend setup

---
+++ backend/animal-intrusion-backend/src/main/java/com/kaif/animalintrusionbackend/AnimalIntrusionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kaif.animalintrusionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalIntrusionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalIntrusionBackendApplication.class, args);
+    }
+
+}
